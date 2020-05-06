bdf172cf3d89ae6fdbbfe535b9d6f4e7f2ace8f8
Yash Garudkar <yashgarudkar@gmail.com>
2020-05-06 15:21:26 +0530

    sign in form done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-up/sign-in-up.component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
 
     </div>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' });
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with you email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name='email'
+                        onChange={this.handleChange}
+                        type='email'
+                        value={this.state.email}
+                        required
+                    />
+                    <label>Email</label>
+                    <input name='password'
+                        onChange={this.handleChange}
+                        type='password'
+                        value={this.state.password}
+                        required
+                    />
+                    <label>Password</label>
+                    <input type='submit' value='Submit' />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
