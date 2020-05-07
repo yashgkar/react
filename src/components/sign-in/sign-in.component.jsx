2dc40bcd6d455150272630ff90da7059cc1e44bf
Yash Garudkar <yashgarudkar@gmail.com>
2020-05-07 20:16:50 +0530

    firebase completed with sign in and sign out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,10 +48,13 @@ class SignIn extends React.Component {
                         label='Password'
                         required
                     />
-                    <CustomButton type='submit'>SIGN IN</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        {' '}Sign In With Google{' '}
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>SIGN IN</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign In With Google
+                        </CustomButton>
+                    </div>
+
                 </form>
             </div>
         )
