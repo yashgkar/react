3b61c1623b13e42d070bd4a790ce007963546692
Yash Garudkar <yashgarudkar@gmail.com>
2020-05-09 13:16:14 +0530

    firestore started

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,18 @@ const config = {
     measurementId: "G-N59WC1MBSZ"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+    }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
